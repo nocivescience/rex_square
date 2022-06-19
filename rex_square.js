90f0551d854dfbd447d61db0f50771b4cac3a719
noceive <nocivescience@gmail.com>
2022-06-19 03:15:20 -0400

    terminada la primera version

--- rex_square.js
+++ rex_square.js
@@ -47,7 +47,7 @@ class Player{
         }else{
             this.dy=0;
             this.grounded=true;
-            this.canvas.height=canvas.height;
+            this.y=canvas.height-this.h;
         }
         this.Draw();
     }
@@ -100,6 +100,81 @@ class Text{
         ctx.fillStyle=this.color;
         ctx.font=this.s+'px Arial';
         ctx.textAlign=this.a;
-        ctx
+        ctx.fillText(this.text,this.x,this.y);
+        ctx.closePath();
     }
 }
+function SpawnObstacle(){
+    let size=RandomIntInRange(20,70);
+    let type=RandomIntInRange(0,1);
+    let obstacle= new Obstacle(canvas.width+size,canvas.height-size,size,size,'red');
+    if(type==1){
+        obstacle.y-=player.originalHeight;
+    }
+    obstacles.push(obstacle);
+}
+function RandomIntInRange(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+function Start(){
+    canvas.width=window.innerWidth;
+    canvas.height=window.innerHeight;
+    ctx.font='30px Arial';
+    gameSpeed=5;
+    gravity=1;
+    score=0;
+    highScore=0;
+    if(localStorage.getItem('highScore')){
+        highScore=localStorage.getItem('highScore');
+    };
+    player=new Player(25,0,50,50,'blue');
+    socreText=new Text('Score: '+score,10,30,'left','white',30);
+    highScoreText=new Text('High Score: '+highScore,10,60,'left','white',30);
+    requestAnimationFrame(Update);
+}
+let initialSpawnTimer=200;
+let spawnTimer=initialSpawnTimer;
+function Update(){
+    requestAnimationFrame(Update);
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    spawnTimer--;
+    if(spawnTimer<=0){
+        SpawnObstacle();
+        console.log(obstacles);
+        spawnTimer=initialSpawnTimer-gameSpeed*8;
+        if(spawnTimer<60){
+            spawnTimer=60;
+        };
+    };
+    //Spawn Enemies
+    for(let i=0;i<obstacles.length;i++){
+        let o=obstacles[i];
+        if(o.x+o.w<0){
+            obstacles.splice(i,1);
+        }
+        if(
+            player.x<o.x+o.w&&
+            player.x+player.width>o.x&&
+            player.y<o.y+o.h&&
+            player.y+player.h>o.y
+        ){
+            obstacles=[];
+            score=0;
+            spawnTimer=initialSpawnTimer;
+            gameSpeed=5;
+            window.localStorage.setItem('highScore',highScore);
+        }
+        o.Update();
+    };
+    player.Animate();
+    score++;
+    socreText.text='Score: '+score;
+    socreText.Draw();
+    if(score>highScore){
+        highScore=score;
+        highScoreText.text='High Score: '+highScore;
+    };
+    highScoreText.Draw();
+    gameSpeed+=0.003;
+};
+Start();
